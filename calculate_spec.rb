45620b020b64afdede13603146d323f7fa31dc44
smakthe <scmakra99@gmail.com>
2024-05-03 14:23:30 +0530

    Added some new testcases

--- calculate_spec.rb
+++ calculate_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Calculator do
     it "returns 0 for empty string" do
         expect(Calculator.add("")).to eq(0)
     end
+    it "returns the sum for the input numbers seperated by comma" do
+        expect(Calculator.add("1,2,3")).to eq(6)
+    end
+    it "rejects any numbers from input which is greater than 1000" do
+        expect(Calculator.add("2,1001")).to eq(2)
+    end
 end
