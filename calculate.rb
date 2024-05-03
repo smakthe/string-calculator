388e49f8f7c517e54d086b33261f316c48864b32
smakthe <scmakra99@gmail.com>
2024-05-03 13:33:26 +0530

    Implented the addition method for comma-seperated inputs

--- calculate.rb
+++ calculate.rb
@@ -4,12 +4,17 @@ module Calculator
     include Matcher
 
     def self.add(input_string)
+        return 0 if input_string.empty?
         if input_string =~ REGEXP
-            puts $1,$2, $3
+            if $1.nil?
+                return $2.split(",").map(&:to_i).reduce(:+)
+            else
+
+            end
         else
             puts "Does not match"
         end
     end
-    
+
 end
-Calculator.add("1,2")
+puts Calculator.add("5")
