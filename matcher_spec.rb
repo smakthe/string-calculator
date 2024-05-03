595d9f6eee6c06e2ca9e98871a2646fcb4959125
smakthe <scmakra99@gmail.com>
2024-05-03 14:33:30 +0530

    Addded new testcase for matcher spec

--- matcher_spec.rb
+++ matcher_spec.rb
@@ -3,10 +3,29 @@ require './matcher'
 include Matcher
 
 RSpec.describe Matcher do
-    it "matches with the valid format" do
+    it "matches with comma-seperated inputs" do
         expect("1,2").to match REGEXP
     end
-    it "does not match when only one number is present" do
+    it "matches when only one number is present" do
+        expect("1").to match REGEXP
+    end
+    it "matches with new line present" do
+        expect("1,
+               2,3").to match REGEXP
+    end
+    it "does not match when numbers are absent after comma" do
         expect("1,").not_to match REGEXP
+    end
+    it "matches with custom delimiter" do
+        expect("//[;]
+               1;2")
+    end
+    it "matches with more than one custom delimiters are present" do
+        expect("//[;][&]
+               1;2&3")
+    end
+    it "matches with multi-character custom delimiters" do
+        expect("//[%][***]
+               1%2***3")
     end
 end
