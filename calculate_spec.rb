17b4f2ad896af8a36999c85fa646fe153456a6f2
smakthe <scmakra99@gmail.com>
2024-07-06 10:24:07 +0530

    Added a few more test cases

--- calculate_spec.rb
+++ calculate_spec.rb
@@ -3,7 +3,16 @@ require './calculate'
 include Calculator
 
 RSpec.describe Calculator do
+    it "returns 0 for empty string" do
+        expect(Calculator.add("")).to eq(0)
+    end
+    it "returns the number when only one number is present" do
+        expect(Calculator.add("1")).to eq(1)
+    end
     it "returns the sum for the input numbers seperated by comma" do
         expect(Calculator.add("1,2,3")).to eq(6)
+    end
+    it "returns the sum if new line present between input numbers" do
+        expect(Calculator.add("1,\n2,3")).to eq(6)
     end
 end
--- matcher_spec.rb
+++ matcher_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Matcher do
     it "matches with comma-seperated inputs" do
         expect("1,2").to match REGEXP
     end
+    it "matches when only one number is present" do
+        expect("1").to match REGEXP
+    end
+    it "matches with new line present" do
+        expect("1,\n2,3").to match REGEXP
+    end
 end
