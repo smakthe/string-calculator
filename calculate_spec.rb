e55c804f184faa518176e6bd7672cfa7fded7022
smakthe <scmakra99@gmail.com>
2024-07-06 10:16:57 +0530

    Added first testcase

--- calculate_spec.rb
+++ calculate_spec.rb
@@ -1,3 +1,9 @@
 require './calculate'
 
 include Calculator
+
+RSpec.describe Calculator do
+    it "returns the sum for the input numbers seperated by comma" do
+        expect(Calculator.add("1,2,3")).to eq(6)
+    end
+end
--- matcher_spec.rb
+++ matcher_spec.rb
@@ -1,3 +1,9 @@
 require './matcher'
 
 include Matcher
+
+RSpec.describe Matcher do
+    it "matches with comma-seperated inputs" do
+        expect("1,2").to match REGEXP
+    end
+end
