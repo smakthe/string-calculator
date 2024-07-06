b88638d9d23a42929272274bd86ace5fa881b2de
smakthe <scmakra99@gmail.com>
2024-07-06 10:33:03 +0530

    Added comments

--- calculate.rb
+++ calculate.rb
@@ -1,20 +1,32 @@
 require './matcher'
 
+# This is the Calculator module, it consists of the add() method which will take the input string
+# and return the expected output. Ex: add("1,2") will return 3
+
 module Calculator
     include Matcher
 
     def self.add(input_string)
+        # If the input is an empty string like this: add(""), then 0 is returned
         return 0 if input_string.empty?
         if input_string =~ REGEXP
+            # Defining a lambda(anonymous function) to process input numbers in an array.
+            # String format numbers in the array are converted to integers.
+            # Input numbers having a value greater than 1000 are rejected.
             rect = ->(arr){arr.map(&:to_i).reject{|n| n > 1000 }}
+            # This is the first scenario with comma-seperated numbers as input without any change to the delimiter.
             if $1.nil?
                 numbers = rect.($2.split(","))
+            # This is the second scenario with custom delimiters.
+            # A regular expression union is created using the delimeters to split the input into array of numbers.
             else
                 numbers = rect.($2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))))
             end
             negetive_values = numbers.select{|n| n < 0 }
+            # Returning all the negetive numbers in case present in input
             if negetive_values.count > 0
                 return "negatives not allowed: "+negetive_values.join(",")
+            # Returning the sum
             else
                 return numbers.reduce(:+)
             end
@@ -22,4 +34,5 @@ module Calculator
             puts "Does not match"
         end
     end
+
 end
--- matcher.rb
+++ matcher.rb
@@ -1,3 +1,30 @@
 module Matcher
+    # This is the regular expression which will match the input string:
+    # ^ asserts position at start of a line
+    # Non-capturing group: (?:\/{2}(\[\D+|\W+|\S+\])\n)*
+    # * matches the previous token between zero and unlimited times
+    # \/ matches the character / literally
+    # {2} matches the previous token exactly 2 times
+    # 1st Capturing Group: (\[\D+|\W+|\S+\])
+    # 1st Alternative: \[\D+
+    # \[ matches the character [ literally
+    # \D matches any character that's not a digit
+    # + matches the previous token between one and unlimited times
+    # 2nd Alternative: \W+
+    # \W matches any non-word character
+    # + matches the previous token between one and unlimited times
+    # 3rd Alternative: \S+\]
+    # \S matches any non-whitespace character
+    # \] matches the character ] literally
+    # \n matches a newline character
+    # 2nd Capturing Group: ((?:\d+(?:\D+|\W+|\S+))*\d+)
+    # Non-capturing group: (?:\d+(?:\D+|\W+|\S+))*
+    # \d matches a digit
+    # Non-capturing group: (?:\D+|\W+|\S+)
+    # 1st Alternative: \D+
+    # 2nd Alternative: \W+
+    # 3rd Alternative: \S+
+    # $ asserts position at the end of a line
+    # Global pattern flag m: multi line
     REGEXP = /^(?:\/{2}(\[\D+|\W+|\S+\])\n)*((?:\d+(?:\D+|\W+|\S+))*\d+)$/m
 end
