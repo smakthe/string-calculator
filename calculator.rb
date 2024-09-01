09b9e2743650b8be0b01c9c6bd028bc4bf356d2f
smakthe <scmakra99@gmail.com>
2024-09-01 20:33:38 +0530

    Added comments

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,37 @@
 require './matcher'
+
+# This is the Calculator module, it consists of the add() method which takes the input string
+# and returns the expected output. Ex: add("1,2") will return 3
+
 module Calculator
     include Matcher
 
     def self.add(input_string)
+        # If the input is an empty string like this: add(""), then 0 is returned
         return 0 if input_string.empty?
+        # If the input string matches the regex, it proceeds with the calculation
         if input_string =~ REGEXP
+            # Defining a lambda (anonymous function) to process input numbers in an array.
+            # String format numbers in the array are converted to integers.
+            # Input numbers having a value greater than 1000 are rejected.
             rect = ->(arr){arr.map(&:to_i).reject{|n| n > 1000 }}
+            # This is the first scenario with comma-seperated numbers as input without any change to the delimiter.
             if $1.nil?
                 numbers = rect.($2.split(","))
+            # This is the second scenario with custom delimiters.
+            # A regular expression union is created using the delimeters to split the input into array of numbers.
             else
                 numbers = rect.($2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))))
             end
             negetive_values = numbers.select{|n| n < 0 }
+            # Returning all the negetive numbers in case present in input
             if negetive_values.count > 0
                 return "negatives not allowed: "+negetive_values.join(",")
+            # Returning the sum
             else
                 return numbers.reduce(:+)
             end
+        # If the input string doesn't match the regex
         else
             puts "Does not match"
         end
--- matcher.rb
+++ matcher.rb
@@ -1,3 +1,30 @@
 module Matcher
+    # This is the regular expression which will match the input string
+    # ^ asserts position at start of a line
+    # This is a non-capturing group: (?:\/{2}(\[\D+|\W+|\S+\])\n)*
+    # * matches the previous token between zero and unlimited times
+    # \/ matches the character "/" literally
+    # {2} matches the previous token exactly 2 times
+    # This is the first capturing Group: (\[\D+|\W+|\S+\])
+    # 1st alternative: \[\D+
+    # \[ matches the character "[" literally
+    # \D matches any character that's not a digit
+    # + matches the previous token between one and unlimited times
+    # 2nd alternative: \W+
+    # \W matches any non-word character
+    # + matches the previous token between one and unlimited times
+    # 3rd alternative: \S+\]
+    # \S matches any non-whitespace character
+    # \] matches the character "]" literally
+    # \n matches a newline character
+    # This is the second capturing Group: ((?:\d+(?:\D+|\W+|\S+))*\d+)
+    # This is a non-capturing group: (?:\d+(?:\D+|\W+|\S+))*
+    # \d matches a digit
+    # This is a non-capturing group: (?:\D+|\W+|\S+)
+    # 1st alternative: \D+
+    # 2nd alternative: \W+
+    # 3rd alternative: \S+
+    # $ asserts position at the end of a line
+    # m: multi-line global pattern flag
     REGEXP = /^(?:\/{2}(\[\D+|\W+|\S+\])\n)*((?:\d+(?:\D+|\W+|\S+))*\d+)$/m
 end
