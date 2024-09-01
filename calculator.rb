45d828c0859a1bc6da05ce648a66fa89a14179ca
smakthe <scmakra99@gmail.com>
2024-09-01 16:34:58 +0530

    Added necessary code to pass the spec

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 module Calculator
     def self.add(input_string)
         return 0 if input_string.empty?
+        return input_string[0].to_i if input_string.length == 1
         return input_string.split(",").map(&:to_i).reduce(:+)
     end
 end
