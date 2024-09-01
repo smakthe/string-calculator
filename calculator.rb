260639aee4a9e10517b6ed5284e9b2684fd9c17b
smakthe <scmakra99@gmail.com>
2024-09-01 16:29:06 +0530

    Added new testcases

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 module Calculator
     def self.add(input_string)
         return 0 if input_string.empty?
+        return input_string.split(",").map(&:to_i).reduce(:+)
     end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -3,6 +3,9 @@ require './calculator'
 include Calculator
 
 RSpec.describe Calculator do
+    it "returns 0 for empty string" do
+        expect(Calculator.add("")).to eq(0)
+    end
     it "returns the sum for the input numbers seperated by comma" do
         expect(Calculator.add("1,2,3")).to eq(6)
     end
