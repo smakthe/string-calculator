cccf6324745c5402eb0b4797a4b21d376366e37c
smakthe <scmakra99@gmail.com>
2024-09-01 20:10:14 +0530

    Added the necessary code for all the testcases to pass

--- calculator.rb
+++ calculator.rb
@@ -1,13 +1,22 @@
 require './matcher'
-
 module Calculator
     include Matcher
 
     def self.add(input_string)
         return 0 if input_string.empty?
         if input_string =~ REGEXP
-            return input_string[0].to_i if input_string.length == 1
-            return input_string.split(",").map(&:to_i).reduce(:+)
+            rect = ->(arr){arr.map(&:to_i).reject{|n| n > 1000 }}
+            if $1.nil?
+                numbers = rect.($2.split(","))
+            else
+                numbers = rect.($2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))))
+            end
+            negetive_values = numbers.select{|n| n < 0 }
+            if negetive_values.count > 0
+                return "negatives not allowed: "+negetive_values.join(",")
+            else
+                return numbers.reduce(:+)
+            end
         else
             puts "Does not match"
         end
