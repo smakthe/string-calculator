394a5ccf27272e3d1b19be5246ccb2a510e087fc
smakthe <scmakra99@gmail.com>
2024-09-01 16:33:36 +0530

    Added new spec

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Calculator do
     it "returns 0 for empty string" do
         expect(Calculator.add("")).to eq(0)
     end
+    it "returns the number when only one number is present" do
+        expect(Calculator.add("1")).to eq(1)
+    end
     it "returns the sum for the input numbers seperated by comma" do
         expect(Calculator.add("1,2,3")).to eq(6)
     end
