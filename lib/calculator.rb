833722acd64d751ecd225697c00ab3eb692da729
smakthe <scmakra99@gmail.com>
2025-03-01 00:25:39 +0530

    🟢 Refactored the add method in the Calculator module

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,8 @@ module Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
     if numbers =~ /^(?:\/{2}(\[\D+|\W+|\S+\])\n)*((?:\d+(?:\D+|\W+|\S+))*\d+)$/m
-      numbers = $1.nil? ? $2.split(",").map(&:to_i).reject{|n| n > 1000 } : $2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))).map(&:to_i).reject{|n| n > 1000 }
+      sanitize = ->(array){ array.map(&:to_i).reject{|number| number > 1000 } }
+      numbers = sanitize.($1.nil? ? $2.split(",") : $2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))))
       negetive_values = numbers.select{|n| n < 0 }
       negetive_values.count > 0 ? "negative numbers not allowed: "+negetive_values.join(",") : numbers.reduce(:+)
     end
