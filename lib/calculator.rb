cf59d6b65399a6d174b80c8a288486aad31a6121
smakthe <scmakra99@gmail.com>
2025-03-01 00:18:44 +0530

    🟢 Added necessary code to make the test for ignoring numbers greater than 1000 pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,7 @@ module Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
     if numbers =~ /^(?:\/{2}(\[\D+|\W+|\S+\])\n)*((?:\d+(?:\D+|\W+|\S+))*\d+)$/m
-      numbers = $1.nil? ? $2.split(",").map(&:to_i) : $2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))).map(&:to_i)
+      numbers = $1.nil? ? $2.split(",").map(&:to_i).reject{|n| n > 1000 } : $2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))).map(&:to_i).reject{|n| n > 1000 }
       negetive_values = numbers.select{|n| n < 0 }
       negetive_values.count > 0 ? "negative numbers not allowed: "+negetive_values.join(",") : numbers.reduce(:+)
     end
