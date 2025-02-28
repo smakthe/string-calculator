1b6906776eb2c8b1866a09e52f455e63c35dde5d
smakthe <scmakra99@gmail.com>
2025-02-28 20:58:28 +0530

    🟢 Added necessary code to make the test for adding two numbers pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,6 +2,7 @@ module Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
     numbers_list = numbers.scan(/\d{1}/)
-    return numbers_list.first.to_i if numbers_list.length < 2 
+    return numbers_list.first.to_i if numbers_list.length < 2
+    numbers_list.map(&:to_i).reduce(:+)
   end
 end
