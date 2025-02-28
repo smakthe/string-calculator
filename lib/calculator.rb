86be8a9aede8763ae155682b4c91ba4b460eede0
smakthe <scmakra99@gmail.com>
2025-02-28 20:53:01 +0530

    🟢 Added necessary code to make the test for only one number pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,7 @@
 module Calculator
   def self.add(numbers)
-    0 if numbers.empty?
+    return 0 if numbers.empty?
+    numbers_list = numbers.scan(/\d{1}/)
+    return numbers_list.first.to_i if numbers_list.length < 2 
   end
 end
