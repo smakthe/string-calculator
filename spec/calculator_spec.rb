3c706388e6be6a757910b85acea96fe048656831
smakthe <scmakra99@gmail.com>
2025-02-28 20:45:25 +0530

    🔴 Added failing test for only one number

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Calculator do
   it "returns 0 for empty string" do
       expect(Calculator.add("")).to eq(0)
   end
+  it "returns the number when only one number is present" do
+    expect(Calculator.add("1")).to eq(1)
+  end
 end
