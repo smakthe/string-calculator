8d7c69db03b96ceed32844f9d7616001da206cad
smakthe <scmakra99@gmail.com>
2025-03-01 00:01:26 +0530

    🟢 Added necessary code to make the test for throwing exception if a negative number is present pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,10 @@
 module Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.scan(/\d{1}/).map(&:to_i).reduce(:+)
+    if numbers =~ /^(?:\/{2}(\[\D+|\W+|\S+\])\n)*((?:\d+(?:\D+|\W+|\S+))*\d+)$/m
+      numbers = $1.nil? ? $2.split(",").map(&:to_i) : $2.split(Regexp.union($1.gsub(/^\[|\]$/,"").split("]["))).map(&:to_i)
+      negetive_values = numbers.select{|n| n < 0 }
+      negetive_values.count > 0 ? "negative numbers not allowed: "+negetive_values.join(",") : numbers.reduce(:+)
+    end
   end
 end
