7e5db13f8af4bbd3e6396b7e2a4f7dc5fd6ffb49
smakthe <scmakra99@gmail.com>
2025-02-28 23:26:03 +0530

    🟢 Added regression test for supporting custom delimiter to verify the current implementation makes the test pass

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,12 +10,15 @@ RSpec.describe Calculator do
   it "returns the sum for two input numbers seperated by comma" do
     expect(Calculator.add("1,2")).to eq(3)
   end
+  it "returns the sum with any amount of numbers present" do
+    expect(Calculator.add("1,2,3,4,5")).to eq(15)
+  end
   it "returns the sum with a newline character persent" do
     expect(Calculator.add("1,
 
                       2,3")).to eq(6)
   end
-  it "returns the sum with any amount of numbers present" do
-    expect(Calculator.add("1,2,3,4,5")).to eq(15)
+  it "returns the sum for the input numbers seperated by a custom delimiter" do
+    expect(Calculator.add("//[;]\n1;2;3")).to eq(6)
   end
 end
