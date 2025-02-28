af97b8a874eb0e094dab4d18c83770b22157251b
smakthe <scmakra99@gmail.com>
2025-02-28 21:03:46 +0530

    🟢 Refactored the add method in the Calculator module

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,8 +1,6 @@
 module Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers_list = numbers.scan(/\d{1}/)
-    return numbers_list.first.to_i if numbers_list.length < 2
-    numbers_list.map(&:to_i).reduce(:+)
+    numbers.scan(/\d{1}/).map(&:to_i).reduce(:+)
   end
 end
